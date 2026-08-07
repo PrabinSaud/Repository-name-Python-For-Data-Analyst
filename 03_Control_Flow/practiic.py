3662fc841e1832abc2078d39e76fbdb371e77461
prabin Saud <prabinsaud2002@gmail.com>
2026-08-07 21:58:34 +0530

    Complete if statement lesson

---
+++ 03_Control_Flow/practiic.py
@@ -0,0 +1,53 @@
+"""
+Module 03 - Control Flow
+Practice
+"""
+
+# ==========================================
+# Topic 1 : If Statement
+# ==========================================
+
+# Practice 1
+#
+# Create:
+# age = 25
+#
+# Print "Adult" if age is 18 or above.
+age = 25
+if age >= 18:
+    print("Adult")
+
+
+# Practice 2
+#
+# Create:
+# marks = 35
+#
+# Print "Pass" if marks are 40 or above.
+marks = 35
+if marks >= 40:
+    print("Pass")
+
+
+
+# Practice 3
+#
+# Create:
+# salary = 75000
+#
+# Print "Bonus Eligible" if salary is greater than 60000.
+salary = 75000
+if salary > 60000:
+    print("Bonus Eligible")
+
+
+
+# Practice 4
+#
+# Create:
+# skills = ["Python", "SQL", "Excel"]
+#
+# Print "SQL Found" if SQL exists in the list.
+skills = ["Python", "SQL", "Excel"]
+if "SQL" in skills:
+    print("SQL Found")  
